4715eeab4e66943497566a0a495ecf0826e232a8
Carlos Quintanar <CQuintanar@mednet.ucla.edu>
2024-09-20 13:36:55 -0700

    update main and variable

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,21 +11,21 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] 
 }
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.Environment.name
+  cidr = "${var.Environment.nework_prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.Environment.nework_prefix}.101.0/24", "${var.Environment.nework_prefix}.102.0/24", "${var.Environment.nework_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.Environment.name
   }
 }
 
@@ -34,8 +34,8 @@ module "autoscaling" {
   version = "8.0.0"
 
   name = "blog"
-  min_size = 1
-  max_size = 2
+  min_size = var.asg_min_size
+  max_size = var.asg.max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   #target_group_arns = module.blog_alb.target_group_arns
@@ -58,7 +58,7 @@ module "blog_alb" {
   security_groups = [module.blog_sg.security_group_id]
 
   target_groups = [{
-    name_prefix      = "blog-"
+    name_prefix      = "${var.Environment.name}-"
     backend_protocol         = "HTTP"
     backend_port     = 80
     target_type      = "instance"
@@ -71,14 +71,14 @@ module "blog_alb" {
   }]
 
   tags = {
-    Environment = "Development"
+    Environment = var.Environment.name
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name = "blog_new"
+  name = ${var.Environment.name}-blog"
 
   vpc_id = module.blog_vpc.vpc_id
 
